0bbf909fc92ac198fbaa9a295d4c1dd231f18069
anantraj07 <anant071123@gmail.com>
2026-05-28 16:57:25 +0530

    Update admin URL path to use settings.ADMIN_PATH

--- isi_welcome/urls.py
+++ isi_welcome/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 admin.site.site_header = "ISI Welcome — Admin"
 admin.site.site_title = "ISI GR Admin"
 admin.site.index_title = "Manage Site Configuration"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(settings.ADMIN_PATH, admin.site.urls),
     path("", include("welcome.urls")),
 ]
